298c4dbf357b7c86c27b28e92ae6f0606b8833d4
martnga <mtnjoroge@gmail.com>
2016-01-26 00:28:50 +0300

    Fixed email bug

--- routes/contact.js
+++ routes/contact.js
@@ -22,7 +22,7 @@ router.post('/send', function(req, res, next) {
     to: 'mtnjoroge@gmail.com',
     subject: 'Website Submission',
     text: 'You have a new submission with the following details ... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
-    html: '<p>You got a new submission with the following details ..</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.name + '</li><li>Message: ' + req.body.message + '</li></ul>'
+    html: '<p>You got a new submission with the following details ..</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
   };
   
   transporter.sendMail(mailOptions, function(error, info){
